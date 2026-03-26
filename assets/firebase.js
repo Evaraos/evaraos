edd060686addb4d946a248fad5544844c52c815e
Evaraos <evaraos.inc@gmail.com>
2026-03-26 16:20:49 -0400

    Add Firebase config
    This file initializes Firebase with the provided configuration and exports authentication and Firestore instances.

---
+++ assets/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAg12tiBifLswke_km3nY6YQpf8ROyqup4",
+  authDomain: "evaraos-web.firebaseapp.com",
+  projectId: "evaraos-web",
+  storageBucket: "evaraos-web.firebasestorage.app",
+  messagingSenderId: "125377381598",
+  appId: "1:125377381598:web:d63df45da3a09f0199ae4f",
+  measurementId: "G-296N94CKPR"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const COMPANY_ID = "supreme-trueclean";
+export const OWNER_EMAIL = "gilbert37ramos@gmail.com";
