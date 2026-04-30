b57a038ebec223fcd3723f429a695560e87c96ce
Evaraos <evaraos.inc@gmail.com>
2026-04-30 02:44:06 -0400

    Export lead privacy sanitizer

--- functions/index-stats.js
+++ functions/index-stats.js
@@ -149,3 +149,4 @@ function attachStats(type) {
 
 ["companies", "users", "leads", "jobs"].forEach(attachStats);
 exports.rebuildStats = require("./stats-rebuild").rebuildStats;
+exports.sanitizeLeadIdentity = require("./lead-privacy").sanitizeLeadIdentity;
