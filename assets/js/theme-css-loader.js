ae2e3212ab5c5617aa620aa7626da80d76274554
Evaraos <evaraos.inc@gmail.com>
2026-04-30 22:35:40 -0400

    Bump install dark polish stylesheet cache

--- assets/js/theme-css-loader.js
+++ assets/js/theme-css-loader.js
@@ -62,7 +62,7 @@
 
   function bootGlobalModules() {
     loadStylesheetOnce("/evaraos/assets/css/install-nav.css?v=5", "evaraInstallNavCss");
-    loadStylesheetOnce("/evaraos/assets/css/install-apple-hotfix.css?v=3", "evaraInstallAppleHotfixCss");
+    loadStylesheetOnce("/evaraos/assets/css/install-apple-hotfix.css?v=4", "evaraInstallAppleHotfixCss");
     loadModuleOnce("/evaraos/assets/js/evara-notifications.js?v=1");
     loadModuleOnce("/evaraos/assets/js/offline-staff-gate.js?v=1");
     loadScriptOnce("/evaraos/assets/js/performance-hotfix.js?v=1", "evaraPerformanceHotfixScript");
