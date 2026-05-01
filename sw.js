900cd6b5a9a7c2e32c534e4373b80aacf22a25c4
Evaraos <evaraos.inc@gmail.com>
2026-05-01 23:57:54 +0000

    Add Dynamic Island nav bridge

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "evaraos-stable-v10";
+const CACHE_VERSION = "evaraos-stable-v11";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 
 const CORE_ASSETS = [
@@ -11,7 +11,7 @@ const CORE_ASSETS = [
   "/manifest.json",
   "/assets/css/base.css?v=34",
   "/assets/css/theme.css?v=36",
-  "/assets/css/nav.css?v=35",
+  "/assets/css/nav.css?v=36",
   "/assets/css/install-nav.css?v=5",
   "/assets/css/home.css?v=34",
   "/assets/css/dashboard.css?v=34",
