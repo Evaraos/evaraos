35dd6a1bdfcf6b39440b69b4118993322cf83c9d
Evaraos <evaraos.inc@gmail.com>
2026-05-02 00:50:59 +0000

    Correct Dynamic Island nav lift clipping

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "evaraos-stable-v17";
+const CACHE_VERSION = "evaraos-stable-v18";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 
 const CORE_ASSETS = [
@@ -11,7 +11,7 @@ const CORE_ASSETS = [
   "/manifest.json",
   "/assets/css/base.css?v=34",
   "/assets/css/theme.css?v=36",
-  "/assets/css/nav.css?v=40",
+  "/assets/css/nav.css?v=41",
   "/assets/css/install-nav.css?v=5",
   "/assets/css/home.css?v=37",
   "/assets/css/dashboard.css?v=34",
