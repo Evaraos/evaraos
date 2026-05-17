8f1fbd70fde886a8f17651b5a3a9ecbbbadac843
Evaraos <evaraos.inc@gmail.com>
2026-05-17 18:31:16 -0400

    Refresh nav JS entrypoint for Image 2 control center

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,1 +1,5 @@
-import "./nav/nav-main.js";
+// Evaraos universal nav entrypoint
+// Version bump forces browsers/PWAs to pull the Image 2 control-center module chain.
+import "./nav/nav-main.js?v=image2-nav-20260517b";
+
+window.EVARAOS_NAV_ENTRY_VERSION = "image2-nav-20260517b";
