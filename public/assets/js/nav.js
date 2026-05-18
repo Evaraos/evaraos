6a7deb065e5d2e30d2aca5b1fb773293dac2f779
Evaraos <evaraos.inc@gmail.com>
2026-05-17 23:08:31 -0400

    Bump nav top entrypoint to image2 runtime d

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,5 +1,5 @@
 // Evaraos universal nav entrypoint
-// Version bump forces browsers/PWAs to pull the Image 2 control-center module chain.
-import "./nav/nav-main.js?v=image2-nav-20260517b";
+// Single source of truth for the Image 2 control-center runtime.
+import "./nav/nav-main.js?v=image2-nav-20260517d";
 
-window.EVARAOS_NAV_ENTRY_VERSION = "image2-nav-20260517b";
+window.EVARAOS_NAV_ENTRY_VERSION = "image2-nav-20260517d";
