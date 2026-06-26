e3c2699dd836713a2e7bd7886bca94bb60c0679d
Evaraos <evaraos.inc@gmail.com>
2026-06-26 13:40:00 -0400

    Switch universal nav to fresh connected runtime

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,5 +1,3 @@
-// Evaraos universal nav entrypoint
-import "./nav/nav-main.js?v=nav-v5";
-import "./nav/nav-essential.js?v=nav-v5";
-
-window.EVARAOS_NAV_ENTRY_VERSION = "nav-v5";
+import "./nav/nav-main-v5.js?v=nav-v5-connected";
+import "./nav/nav-essential.js?v=nav-v5-connected";
+window.EVARAOS_NAV_ENTRY_VERSION="nav-v5-connected";
