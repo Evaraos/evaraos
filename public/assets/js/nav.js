0f1448e0fa03aa8039966302314841614e73de73
Evaraos <evaraos.inc@gmail.com>
2026-06-26 16:53:02 -0400

    Publish nav-v12 drawer cleanup

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,7 +1,7 @@
-import "./nav/nav-main-v5.js?v=nav-v11-icon-system";
-import "./nav/nav-bottom.js?v=nav-v11-icon-system";
-import "./nav/nav-drawer-close.js?v=nav-v11-icon-system";
-import "./nav/pull-to-refresh.js?v=nav-v11-icon-system";
-import "./nav/avatar-sync.js?v=nav-v11-icon-system";
+import "./nav/nav-main-v5.js?v=nav-v12-drawer-cleanup";
+import "./nav/nav-bottom.js?v=nav-v12-drawer-cleanup";
+import "./nav/nav-drawer-close.js?v=nav-v12-drawer-cleanup";
+import "./nav/pull-to-refresh.js?v=nav-v12-drawer-cleanup";
+import "./nav/avatar-sync.js?v=nav-v12-drawer-cleanup";
 import "./ui/icon-hydrator.js?v=1";
-window.EVARAOS_NAV_ENTRY_VERSION="nav-v11-icon-system";
+window.EVARAOS_NAV_ENTRY_VERSION="nav-v12-drawer-cleanup";
