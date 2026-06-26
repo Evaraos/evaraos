7109d291b7de42976d8d3e1236220ed0dca852b1
Evaraos <evaraos.inc@gmail.com>
2026-06-26 14:10:37 -0400

    Finish dual-nav drawer interactions

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,3 +1,4 @@
 import "./nav/nav-main-v5.js?v=nav-v7-dual-nav";
 import "./nav/nav-bottom.js?v=nav-v7-dual-nav";
+import "./nav/nav-drawer-close.js?v=nav-v7-dual-nav";
 window.EVARAOS_NAV_ENTRY_VERSION="nav-v7-dual-nav";
