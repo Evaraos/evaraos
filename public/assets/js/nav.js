debb2390b401231c6ae3fc4884cce50fb6d247ac
Evaraos <evaraos.inc@gmail.com>
2026-06-26 14:08:47 -0400

    Restore bottom nav beside top profile navigation

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,2 +1,3 @@
-import "./nav/nav-main-v5.js?v=nav-v6-profile-drawer";
-window.EVARAOS_NAV_ENTRY_VERSION="nav-v6-profile-drawer";
+import "./nav/nav-main-v5.js?v=nav-v7-dual-nav";
+import "./nav/nav-bottom.js?v=nav-v7-dual-nav";
+window.EVARAOS_NAV_ENTRY_VERSION="nav-v7-dual-nav";
