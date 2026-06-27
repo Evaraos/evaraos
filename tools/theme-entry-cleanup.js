b6e3e96a3b5f7d070decde54664988455bddedb5
Evaraos <evaraos.inc@gmail.com>
2026-06-27 01:06:54 -0400

    Remove obsolete theme cleanup patcher

--- tools/theme-entry-cleanup.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-const fs=require("fs"),path=require("path");
-const file=path.resolve(__dirname,"../public/assets/css/theme.css");
-const before=fs.readFileSync(file,"utf8");
-const after=before.replace(/^.*apple-settings-glass\.css.*\n?/m,"");
-if(after!==before){fs.writeFileSync(file,after,"utf8");console.log("Removed dead settings glass import.");}
