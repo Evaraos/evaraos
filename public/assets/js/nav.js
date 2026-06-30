2b5ddb76a9d9199b17e034fef032315316cd2a5f
Evaraos <evaraos.inc@gmail.com>
2026-06-30 16:52:12 -0400

    Load X-style drawer runtime v25

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,7 +1,7 @@
-import "./nav/nav-main-v5.js?v=nav-v24-x-hybrid-drawer";
-import "./nav/nav-bottom.js?v=nav-v24-x-hybrid-drawer";
-import "./nav/nav-drawer-close.js?v=nav-v24-x-hybrid-drawer";
-import "./nav/pull-to-refresh.js?v=nav-v24-x-hybrid-drawer";
-import "./nav/avatar-sync.js?v=nav-v24-x-hybrid-drawer";
+import "./nav/nav-main-v5.js?v=nav-v25-x-drawer";
+import "./nav/nav-bottom.js?v=nav-v25-x-drawer";
+import "./nav/nav-drawer-close.js?v=nav-v25-x-drawer";
+import "./nav/pull-to-refresh.js?v=nav-v25-x-drawer";
+import "./nav/avatar-sync.js?v=nav-v25-x-drawer";
 import "./ui/icon-hydrator.js?v=2";
-window.EVARAOS_NAV_ENTRY_VERSION="nav-v24-x-hybrid-drawer";
+window.EVARAOS_NAV_ENTRY_VERSION="nav-v25-x-drawer";
