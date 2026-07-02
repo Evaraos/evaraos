c97d84da24ab24ec3a1606ad4b0567e1f48e6332
Evaraos <evaraos.inc@gmail.com>
2026-07-02 19:24:56 -0400

    Use canonical adaptive theme v9 runtime

--- public/assets/js/theme.js
+++ public/assets/js/theme.js
@@ -3,13 +3,13 @@ import { installOnboardingEntry } from "./onboarding-entry.js?v=2";
 import "./staff-application-dedupe.js?v=2";
 import "./staff-application-experience.js?v=1";
 import "./social-links.js?v=1";
-import * as Core from "./theme-core-adaptive.js?v=adaptive-liquid-v8";
+import * as Core from "./theme-core-adaptive.js?v=adaptive-liquid-v9";
 
 installLiquidInteraction();
 installOnboardingEntry();
 
-export const EVARAOS_THEME_BUILD = "adaptive-liquid-v8";
-export * from "./theme-core-adaptive.js?v=adaptive-liquid-v8";
+export const EVARAOS_THEME_BUILD = "adaptive-liquid-v9";
+export * from "./theme-core-adaptive.js?v=adaptive-liquid-v9";
 
 function enforceUniversalTheme(detail = {}) {
   const root = document.documentElement;
