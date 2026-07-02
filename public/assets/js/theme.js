41f543b2645d47d69b109c2c8613f3a1e776a39c
Evaraos <evaraos.inc@gmail.com>
2026-07-01 22:39:17 -0400

    Point theme entry to multi-mode optical runtime v7

--- public/assets/js/theme.js
+++ public/assets/js/theme.js
@@ -1,2 +1,2 @@
-// Single adaptive image theme entry.
-export * from "./theme-core-adaptive.js?v=adaptive-liquid-v6";
+export const EVARAOS_THEME_BUILD="adaptive-liquid-v7";
+export * from "./theme-core-adaptive.js?v=adaptive-liquid-v7";
