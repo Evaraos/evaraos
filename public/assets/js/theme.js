7b0594b90f3f9ae2c3a57be70a7592a88edc0796
Evaraos <evaraos.inc@gmail.com>
2026-07-01 22:40:00 -0400

    Initialize optical refraction from the theme entry

--- public/assets/js/theme.js
+++ public/assets/js/theme.js
@@ -1,2 +1,4 @@
+import { installLiquidOptics } from "./liquid-optics.js?v=1";
+installLiquidOptics();
 export const EVARAOS_THEME_BUILD="adaptive-liquid-v7";
 export * from "./theme-core-adaptive.js?v=adaptive-liquid-v7";
