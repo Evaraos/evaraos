f0f9e7731a7dd705d9747ccb9b8d645600cb45b6
Evaraos <evaraos.inc@gmail.com>
2026-07-02 23:30:52 -0400

    Fix Firebase Messaging service worker configuration

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,9 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
+  apiKey: "AIzaSyAg12tiBifLswke_km3nY6YQpf8ROyqup4",
+  authDomain: "evaraos-web.firebaseapp.com",
   projectId: "evaraos-web",
+  storageBucket: "evaraos-web.firebasestorage.app",
   messagingSenderId: "125377381598",
-  appId: "1:125377381598:web:d63df45da3a09f0199ae4f"
+  appId: "1:125377381598:web:d63df45da3a09f0199ae4f",
+  measurementId: "G-296N94CKPR"
 });
 
 const messaging = firebase.messaging();
@@ -30,6 +34,7 @@ self.addEventListener("notificationclick", (event) => {
   event.notification.close();
   const route = event.notification?.data?.route || "/messages.html";
   const destination = new URL(route, self.location.origin).href;
+
   event.waitUntil((async () => {
     const windows = await clients.matchAll({ type: "window", includeUncontrolled: true });
     for (const client of windows) {
