86ab76e1c1496801eef919090215f3112efe8803
Evaraos <evaraos.inc@gmail.com>
2026-07-07 16:43:37 -0400

    Load permission runtime with navigation

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,8 +1,9 @@
-import "./nav/nav-main-v6.js?v=nav-v40-role-lockdown";
-import "./nav/nav-bottom.js?v=nav-v40-role-lockdown";
-import "./nav/nav-drawer-close.js?v=nav-v40-role-lockdown";
-import "./nav/pull-to-refresh.js?v=nav-v40-role-lockdown";
-import "./nav/avatar-sync.js?v=nav-v40-role-lockdown";
-import "./ui/icon-hydrator.js?v=8";
-import "./nav/nav-role-lockdown.js?v=1";
-window.EVARAOS_NAV_ENTRY_VERSION = "nav-v40-role-lockdown";
+import "./nav/nav-main-v6.js?v=nav-v41-permissions";
+import "./nav/nav-bottom.js?v=nav-v41-permissions";
+import "./nav/nav-drawer-close.js?v=nav-v41-permissions";
+import "./nav/pull-to-refresh.js?v=nav-v41-permissions";
+import "./nav/avatar-sync.js?v=nav-v41-permissions";
+import "./ui/icon-hydrator.js?v=9";
+import "./nav/nav-role-lockdown.js?v=2";
+import "./permissions-runtime.js?v=1";
+window.EVARAOS_NAV_ENTRY_VERSION = "nav-v41-permissions";
