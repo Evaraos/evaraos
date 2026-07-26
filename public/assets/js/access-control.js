34b39a24bbf11add61cab70b766cf100d292a6a5
Evaraos <evaraos.inc@gmail.com>
2026-07-26 08:45:48 -0400

    Preserve explicit access-control export contracts

--- public/assets/js/access-control.js
+++ public/assets/js/access-control.js
@@ -1,1 +1,19 @@
-export * from './access-control-v2.js?v=access-control-v2';
+export {
+  CANONICAL_ROLES,
+  PUBLIC_PAGES,
+  PAGE_POLICY,
+  FEATURE_POLICY,
+  normalizeAccessRole,
+  isKnownRole,
+  isPlatformAdmin,
+  isPlatformOwner,
+  isTenantExecutive,
+  isTenantLeadership,
+  isFieldRole,
+  canUseFeature,
+  canAccessPageName,
+  defaultRouteForRole,
+  permissionsForRole,
+  pagesForRole,
+  accessDecision
+} from './access-control-v2.js?v=access-control-v2';
