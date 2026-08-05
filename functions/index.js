98c9cabf5acd574390d9dad029c8a6b480c82e92
Evaraos <evaraos.inc@gmail.com>
2026-08-05 03:56:58 -0400

    fix: remove temporary Functions aggregator

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const existingFunctions = require('./index-stats');
-const experienceFunctions = require('./experience-config-service');
-
-module.exports = {
-  ...existingFunctions,
-  ...experienceFunctions
-};
